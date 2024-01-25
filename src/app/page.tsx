6c940111f949ed90b31706a60157d643a768227d
Ziyang Wen <wenziyang007@gmail.com>
2024-01-25 16:48:32 +0800

    fetching data with server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+import { db } from "@/db"
 
-export default function Home() {
+export default async function Home() {
+  const snippets = await db.snippet.findMany()
+
+  const renderedSnippets = snippets.map((snippet) => {
+    return <div key={snippet.id}>{snippet.title}</div>
+})
+
   return (
-    <div>Home Page</div>
+    <div>{renderedSnippets}</div>
     )
   }
